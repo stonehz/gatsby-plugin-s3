38a109c574d0b8ce27830090dc66daa265e988fa
Tony Ntas <antonios.ntasioudis@ascential.com>
2019-07-19 14:12:24 +0100

    handle external redirects in s3rules.

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -23,30 +23,30 @@ const getRules = (pluginOptions: PluginOptions, routes: GatsbyRedirect[]): Routi
         },      
     }))
 );
-const buildCondition = (path: string) => {
+const buildCondition = (redirectPath: string) => {
     return {
-        KeyPrefixEquals: withoutLeadingSlash(path),
+        KeyPrefixEquals: withoutLeadingSlash(redirectPath),
         HttpErrorCodeReturnedEquals: '404',
     };
 };
 
 const buildRedirect = (pluginOptions: PluginOptions, route: GatsbyRedirect) => {
     if (route.toPath.indexOf('://') > 0) {
-      const url = new URL(route.toPath);
-      return {
-        ReplaceKeyWith: withoutTrailingSlash(withoutLeadingSlash(route.toPath)),
-        HttpRedirectCode: route.isPermanent ? '301' : '302',
-        Protocol: url.protocol,
-        HostName: url.hostname,
-    };
-  } else {
-      return {
-        ReplaceKeyWith: withoutTrailingSlash(withoutLeadingSlash(route.toPath)),
-        HttpRedirectCode: route.isPermanent ? '301' : '302',
-        Protocol: pluginOptions.protocol,
-        HostName: pluginOptions.hostname,
-    };
-  }
+        const url = new URL(route.toPath);
+        return {
+            ReplaceKeyWith: withoutTrailingSlash(withoutLeadingSlash(url.href.replace(url.origin, ''))),
+            HttpRedirectCode: route.isPermanent ? '301' : '302',
+            Protocol: url.protocol.slice(0, -1),
+            HostName: url.hostname,
+        };
+    } else {
+        return {
+            ReplaceKeyWith: withoutTrailingSlash(withoutLeadingSlash(route.toPath)),
+            HttpRedirectCode: route.isPermanent ? '301' : '302',
+            Protocol: pluginOptions.protocol,
+            HostName: pluginOptions.hostname,
+        };
+    }
 };
 
 let params: Params = {};
@@ -93,7 +93,7 @@ export const onPostBuild = ({ store }: any, userPluginOptions: PluginOptions) =>
     const pluginOptions = { ...DEFAULT_OPTIONS, ...userPluginOptions };
     const { redirects, pages, program }: GatsbyState = store.getState();
 
-    if (!pluginOptions.hostname != !pluginOptions.protocol) { // If one of these is provided but not the other
+    if (!pluginOptions.hostname !== !pluginOptions.protocol) { // If one of these is provided but not the other
         throw new Error(`Please either provide both 'hostname' and 'protocol', or neither of them.`);
     }
 
